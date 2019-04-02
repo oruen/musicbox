8ab5c24cf70cf02a943b1c7e034c4966375cd78a
Nikolai Rekubratskii <nikolai.rekubratskii@xing.com>
2019-04-02 13:18:36 +0200

    Make in work.

--- player_launcher.sh
+++ player_launcher.sh
@@ -3,4 +3,4 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /home/pi
-sudo -u pi ./scan_for_tags.py
+sudo -u pi ./scan_for_tags.py > /home/pi/player.log 2>&1
--- scan_for_tags.py
+++ scan_for_tags.py
@@ -9,6 +9,7 @@ from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_nfc_rfid import BrickletNFCRFID
 import requests
 import time
+import sys
 
 tag_type = 0
 last_seen_tag = None
@@ -32,45 +33,52 @@ def play_song(song_id):
 
 # Callback function for state changed callback
 def cb_state_changed(state, idle, nr):
-    if state == nr.STATE_REQUEST_TAG_ID_READY:
-        ret = nr.get_tag_id()
-        global last_seen_tag
-        global tag_a
-        if ret != last_seen_tag:
-            #print("Found tag of type " + str(ret.tag_type) + " " + str(ret) + " with ID [" +
-            #      " ".join(map(str, map(hex, ret.tid[:ret.tid_length]))) + "]")
-            if tag_black == ret.tid:
-                print("Tag Black")
-                play_song(1)
-            elif tag_red == ret.tid:
-                print("Tag Red")
-                play_song(2)
-            else:
-                print("Unknown tag")
-        last_seen_tag = ret
+    try:
+        if state == nr.STATE_REQUEST_TAG_ID_READY:
+            ret = nr.get_tag_id()
+            global last_seen_tag
+            global tag_a
+            if ret != last_seen_tag:
+                #print("Found tag of type " + str(ret.tag_type) + " " + str(ret) + " with ID [" +
+                #      " ".join(map(str, map(hex, ret.tid[:ret.tid_length]))) + "]")
+                if tag_black == ret.tid:
+                    print("Tag Black")
+                    play_song(1)
+                elif tag_red == ret.tid:
+                    print("Tag Red")
+                    play_song(2)
+                else:
+                    print("Unknown tag")
+            last_seen_tag = ret
 
-    # Cycle through all types
-    if idle:
-        global tag_type
-        tag_type = (tag_type + 1) % 3
-        nr.request_tag_id(tag_type)
+        # Cycle through all types
+        if idle:
+            global tag_type
+            tag_type = (tag_type + 1) % 3
+            nr.request_tag_id(tag_type)
+    except:
+        sys.exit("Error occured")
 
 if __name__ == "__main__":
-    ipcon = IPConnection() # Create IP connection
-    nr = BrickletNFCRFID(UID, ipcon) # Create device object
+    try:
+        time.sleep(10)
+        ipcon = IPConnection() # Create IP connection
+        nr = BrickletNFCRFID(UID, ipcon) # Create device object
 
-    ipcon.connect(HOST, PORT) # Connect to brickd
-    # Don't use device before ipcon is connected
+        ipcon.connect(HOST, PORT) # Connect to brickd
+        # Don't use device before ipcon is connected
 
-    # Register state changed callback to function cb_state_changed
-    nr.register_callback(nr.CALLBACK_STATE_CHANGED,
-                         lambda x, y: cb_state_changed(x, y, nr))
+        # Register state changed callback to function cb_state_changed
+        nr.register_callback(nr.CALLBACK_STATE_CHANGED,
+                             lambda x, y: cb_state_changed(x, y, nr))
 
-    # Start scan loop
-    nr.request_tag_id(nr.TAG_TYPE_MIFARE_CLASSIC)
+        # Start scan loop
+        nr.request_tag_id(nr.TAG_TYPE_MIFARE_CLASSIC)
 
-    #raw_input("Press key to exit\n") # Use input() in Python 3
-    while True:
-        time.sleep(1)
+        #raw_input("Press key to exit\n") # Use input() in Python 3
+        while True:
+            time.sleep(1)
+    except:
+        system.exit("Error")
         # hello
     #ipcon.disconnect()
